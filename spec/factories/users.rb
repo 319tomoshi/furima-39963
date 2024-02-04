8b8ee1e0814063cf20556fce85e213284c374237
319tomoshi <rhimssiictiic@gmail.com>
2024-02-05 00:48:57 +0900

    FactoryBotとFaker

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :user do
+    nickname               { Faker::Name.nickname }
+    email                  { Faker::Internet.email }
+    password               { 'password123' }
+    password_confirmation  { password }
+    last_name              { '中村' }
+    first_name             { '知思' }
+    last_name_kana         { 'ナカムラ' }
+    first_name_kana        { 'トモシ' }
+    date_of_birth          { Faker::Date.birthday }
