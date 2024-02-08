cdb7a7eaf057fe9c245cfb5081c2c04f58c7c6f7
319tomoshi <rhimssiictiic@gmail.com>
2024-02-08 18:20:20 +0900

    画像を保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:name, :description)
+    params.require(:item).permit(:name, :description, :image)
   end
 
 end
