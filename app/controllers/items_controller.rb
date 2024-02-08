d5b611a103c68b99c3fe29a960fd64bd8793f0e2
319tomoshi <rhimssiictiic@gmail.com>
2024-02-08 17:39:17 +0900

    商品名と商品の説明

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
-  def index
+
+  def new
+    @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description)
+  end
+
 end
