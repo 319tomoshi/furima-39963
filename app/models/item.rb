bc19e7d613b01a40b655d3a4aac01d592e8fb07a
319tomoshi <rhimssiictiic@gmail.com>
2024-02-10 01:06:13 +0900

    モデル名の誤りを修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,13 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :shpping_fee_paid_by
+  belongs_to :shipping_fee_paid_by
   belongs_to :origin_region
   belongs_to :shipping_day
 
   has_one_attached :image
 
-  validates :name, :description, presence: true
+  validates :name, :description, :price, presence: true
 
   validates :category_id, :condition_id, :shipping_fee_paid_by_id, :origin_region_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
