bba722f1b0bc13c11fafca63990adb169f096303
319tomoshi <rhimssiictiic@gmail.com>
2024-02-09 17:03:16 +0900

    商品の詳細と配送について

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,13 +5,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to '/'
+    @item = Item.new(item_params)
+    if @article.save
+      redirect_to '/'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:name, :description, :image)
+    params.require(:item).permit(:name, :description, :image, :category_id, :condition_id, :shpping_fee_paid_by_id, :origin_region_id, :shiopping_day_id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shpping_fee_paid_by
+  belongs_to :origin_region
+  belongs_to :shipping_day
+
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :description, presence: true
+  validates :name, :description, presence: true
+
+  validates :category_id, :condition_id, :shpping_fee_paid_by_id, :origin_region_id, :shiopping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 end
