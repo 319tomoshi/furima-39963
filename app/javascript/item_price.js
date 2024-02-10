14b71bb3b6bc3bae59088a31fd2c754e6331fc75
319tomoshi <rhimssiictiic@gmail.com>
2024-02-11 00:06:34 +0900

    販売価格

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,23 +1,27 @@
-const priceInput = document.getElementById("item-price");
-const addTaxDom = document.getElementById("add-tax-price");
-const profitDom = document.getElementById("profit");
+function calculateSellingPrice (){
+  const priceInput = document.getElementById("item-price");
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
 
-priceInput.addEventListener("input", () => {
-  const inputValue = parseFloat(priceInput.value);
-  const tax = Math.floor(inputValue * 0.1);
-  const profit = Math.floor(inputValue - tax);
+  priceInput.addEventListener("input", () => {
+    const inputValue = parseFloat(priceInput.value);
+    const tax = Math.floor(inputValue * 0.1);
+    const profit = Math.floor(inputValue - tax);
 
-  if (isNaN(inputValue) || inputValue < 0) {
-    addTaxDom.innerHTML = '';
-    profitDom.innerHTML = '';
-    return;
-  }
+    if (isNaN(inputValue) || inputValue < 0) {
+      addTaxDom.innerHTML = '';
+      profitDom.innerHTML = '';
+      return;
+    }
 
-  if (inputValue >= 300) {
-    addTaxDom.innerHTML = `${tax}`;
-    profitDom.innerHTML = `${profit}`
-  } else {
-    addTaxDom.innerHTML = '';
-    profitDom.innerHTML = '';
-  }
-})
+    if (inputValue >= 300) {
+      addTaxDom.innerHTML = `${tax}`;
+      profitDom.innerHTML = `${profit}`;
+    } else {
+      addTaxDom.innerHTML = '';
+      profitDom.innerHTML = '';
+    }
+  })
+};
+
+window.addEventListener('turbo:load', calculateSellingPrice)
