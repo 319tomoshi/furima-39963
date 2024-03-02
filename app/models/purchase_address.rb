3d06b9e6183d7252092b2473641dfe1dac2839ac
319tomoshi <rhimssiictiic@gmail.com>
2024-03-02 21:02:54 +0900

    テストコード

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -3,8 +3,11 @@ class PurchaseAddress
   attr_accessor :user_id, :item_id, :purchase_id, :postal_code, :origin_region_id, :city, :street_number, :building_name, :phone_number
     
   with_options presence: true do
-    validates :user_id, :item_id, :postal_code, :origin_region_id, :city, :street_number, :phone_number
+    validates :user_id, :item_id, :city, :street_number
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, numericality: { only_integer: true, message: "must be an integer with 10 to 11 digits" }, length: { minimum: 10, maximum: 11, message: "must be an integer with 10 to 11 digits" }
   end
+  validates :origin_region_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -3,44 +3,73 @@ require 'rails_helper'
 RSpec.describe PurchaseAddress, type: :model do
   before do
     purchase = FactoryBot.create(:purchase)
-    @purchase_address = FactoryBot.build(:purchase_address, purchase_id: purchase.id)
+    user = FactoryBot.create(:user)
+    item = FactoryBot.create(:item)
+    @purchase_address = FactoryBot.build(:purchase_address, purchase_id: purchase.id, user_id: user.id, item_id: item.id)
   end
 
   describe '配送先情報の保存' do
 
     context '入力に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できる' do
-        sleep(3)
+        expect(@purchase_address).to be_valid
+        sleep(1)
       end
       it 'building_nameは空でも保存できる' do
-        sleep(3)
+        @purchase_address.building_name = ''
+        expect(@purchase_address).to be_valid
+        sleep(1)
       end
     end
 
     context '入力に問題ある場合' do
       it 'postal_codeが空だと保存できない' do
-        sleep(3)
+        @purchase_address.postal_code = ''
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("Postal code can't be blank")
       end
       it 'postal_codeが半角のハイフンを含んだ正しい形式でないと保存できない' do
-        sleep(3)
+        @purchase_address.postal_code = '1234567'
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
       end
       it 'origin_region_idが空だと保存できない' do
-        sleep(3)
+        @purchase_address.origin_region_id = '1'
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("Origin region can't be blank")
       end
       it 'cityが空だと保存できない' do
-        sleep(3)
+        @purchase_address.city = ''
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("City can't be blank")
       end
       it 'street_numberが空だと保存できない' do
-        sleep(3)
+        @purchase_address.street_number = ''
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("Street number can't be blank")
       end
       it 'phone_numberが空だと保存できない' do
-        sleep(3)
+        @purchase_address.phone_number = ''
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
       end
       it 'phone_numberが半角数字以外が含まれている場合は保存できない' do
-        sleep(3)
+        @purchase_address.phone_number = '０７０１２３４５６７８'
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("Phone number must be an integer with 10 to 11 digits")
       end
       it 'phone_numberが10桁以上11桁以内でないと保存できない' do
-        sleep(3)
+        @purchase_address.phone_number = '070123456789'
+        @purchase_address.valid?
+        sleep(1)
+        expect(@purchase_address.errors.full_messages).to include("Phone number must be an integer with 10 to 11 digits")
       end
     end
   end
